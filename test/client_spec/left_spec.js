2ef519b975afef52e598f6b6d4375b36ce186bd0
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-29 00:38:20 -0800

    Fixed file system to take advantage of parent child communication

--- test/client_spec/left_spec.js
+++
@@ -1,9 +0,0 @@
-// <!-- Created by Duncan on 12.28.2016 -->
-const Vue = require('vue');
-const Left = require('../../client/vue-components/left.vue');
-
-describe('Left', () => {
-  it('should exist', () => {
-    expect(typeof Left).toBe('object');
-  })
-});
