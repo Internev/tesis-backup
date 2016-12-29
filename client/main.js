8cc6868d60a0eaf6738c451aaac25c8061c649fb
Duncan Nevin <duncan.nevin@gmail.com>
2016-12-28 18:28:41 -0800

    Added packages for jasmine testing

--- client/main.js
+++ client/main.js
@@ -4,17 +4,20 @@ import Navbar from './vue-components/navbar.vue'
 import Left from './vue-components/left.vue'
 import Right from './vue-components/right.vue'
 
-new Vue({
-  el: 'navbar',
-  components: { Navbar }
-})
+  new Vue({
+    el: 'navbar',
+    components: { Navbar }
+  }),
 
-new Vue({
-  el: 'left',
-  components: { Left }
-})
+  new Vue({
+    el: 'left',
+    components: { Left }
+  })
 
-new Vue({
-  el: 'right',
-  components: { Right }
-})
+  new Vue({
+    el: 'right',
+    components: { Right }
+  })
+
+module.exports = {}
+
